51151fe8d3b5c51e3500d94130284172831ed8d2
Angeli Imre <angeliimre577@gmail.com>
2022-11-06 23:02:34 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import {beolvas,szures,megjelenit,eltuntet} from './productSlice';
+import productSlice from './productSlice';
+import {useSelector,useDispatch} from 'react-redux'
+import { useEffect } from 'react';
+import {useState} from 'react'
+
+function DetailBox(){
+  const dispatch=useDispatch()
+  const detail=useSelector(state=>state.product)
+  return<div onClick={function(){dispatch(eltuntet())}} style={{position:"fixed",left:"0px",top:"0px",backgroundColor:"rgba(0,0,0,0.5)",display:"flex",justifyContent:"center",alignItems:"center",height:"100vh",width:"100%",visibility:detail.show}}>
+    <div style={{width:"30%",backgroundColor:"white",padding:"15px",borderRadius:"15px"}} onClick={function(e){e.stopPropagation()}}>
+      <b>{detail.kivalasztott.title}</b>
+      <p>{detail.kivalasztott.description}</p>
+      <img src={detail.kivalasztott.images[1]} style={{maxWidth:"100%"}}/>
+    </div>
+  </div>
+}
+
+function Kategoria(){
+  const detail=useSelector(state=>state.product)
+  const dispatch=useDispatch()
+
+  const arr=[]
+
+  return<div style={{marginLeft:"20px"}}>
+    {detail.status=="loading"?"betoltes":detail.value.map(function(element){
+      if(arr.indexOf(element.category)==-1)
+        arr.push(element.category)
+      
+    })}
+    {
+      arr.map(function(elem){
+        return <button onClick={function(){
+          dispatch(szures(elem))
+        }}>{elem}</button>
+      })
+    }
+  </div>
+}
+function Termekek(){
+  const detail=useSelector(state=>state.product)
+  const dispatch=useDispatch()
+
+  return(detail.szurt.length>0?<div style={detail.szurt[0].category=="smartphones"||detail.szurt[0].category=="laptops"?{display:'flex',flexWrap:'wrap',justifyContent:'space-between',width:'90%', marginLeft: 'auto',marginRight:'auto', border:'2px solid black'}:{display:'flex',flexWrap:'wrap',flexDirection:'column',justifyContent:'space-between',width:'30%', marginLeft: 'auto',marginRight:'auto', border:'2px solid black'}}>
+        {
+          detail.szurt.map(function(element){
+            return <div style={{flexBasis:'30%',marginBottom: "20px",border:'1px solid black',textAlign:'center'}}>
+              <img src={element.images[0]} style={{height: "200px",maxWidth:"100%"}}/>
+              <h2 style={{width:'100%',textAlign:'left'}}>
+                {element.title+"   $"+element.price}
+              </h2>
+              <button style={{background:"blue",margin:"20px",padding:"10px",fontSize: "20px",border:"none", borderRadius:"5px",color:"white"}} onClick={()=>{dispatch(megjelenit(element))}}>Részletek</button>
+            </div>
+          })
+        }
+      </div>:"")
+}
 
 function App() {
+  const detail=useSelector(state=>state.product)
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    dispatch(beolvas())
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        {detail.status=="loading"?"betoltes":<>
+        
+          <Kategoria></Kategoria>
+          <Termekek></Termekek>
+          <DetailBox></DetailBox>
+        </>}
+      
+    </>
   );
 }
 
---
+++ src/productSlice.js
@@ -0,0 +1,45 @@
+import {createSlice,createAsyncThunk} from "@reduxjs/toolkit"
+
+const productSlice=createSlice({
+    name:"product",
+    initialState:{
+        kivalasztott: {},
+        value:[],
+        status: "loading",
+        szurt:[],
+        show:"hidden"
+    },
+    extraReducers:make=>make.addCase(beolvas.pending,function(state){
+        state.status="loading"
+    }).addCase(beolvas.fulfilled,(state,action)=>{
+        state.status="ok";
+        state.value=action.payload.products
+    }),
+    reducers:{
+        szures:(state,action)=>{
+            state.szurt=[]
+            state.value.forEach(function(item){
+                if(item.category==action.payload){
+                    state.szurt.push(item)
+                    state.szurt=[...state.szurt]
+                }
+            })
+        },
+        megjelenit:(state,action)=>{
+            state.show="visible";
+            state.kivalasztott=action.payload
+        },
+        eltuntet:(state)=>{
+            state.show="hidden"
+        }
+    }
+})
+
+const beolvas=createAsyncThunk("products/get",async()=>{
+    const f=await fetch("https://kepzes.appworld.hu/api/products.php")
+    return f.json()
+})
+
+export default productSlice.reducer
+export {beolvas}
+export const {szures,megjelenit,eltuntet}=productSlice.actions
